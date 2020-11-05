2210a2be75c01423af496d90fb037e5bdf53eee1
Demongo2009 <kuba.galat1@wp.pl>
2020-11-05 08:43:45 +0100

    initialize Clion

---
+++ cmake-build-debug/CMakeFiles/PSZT_MemeticAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSZT_MemeticAlgorithm.dir/main.cpp.o"
+  "PSZT_MemeticAlgorithm"
+  "PSZT_MemeticAlgorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSZT_MemeticAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
